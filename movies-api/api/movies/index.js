1260bfd8b6acd52ecd275fba84868ce904972f86
Hiroto Iwata <hiroto1610@outlook.com>
2022-11-25 21:12:07 +0000

    added routing for movies and movie reviews

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 import {movies,movieDetails,movieReviews} from "./moviesData";
+import uniqid from 'uniqid'
 
 const moviesRouter = express.Router();
+moviesRouter.get('/', (req, res) => {
+    res.json(movies);
+});
 moviesRouter.get('/:id',(req,res)=>{
     const id = parseInt(req.params.id);
     if (movieDetails.id === id) {
@@ -13,4 +17,31 @@ moviesRouter.get('/:id',(req,res)=>{
         });
     }
 });
+moviesRouter.get('/:id/reviews',(req,res) =>{
+    const id = parseInt(req.params.id);
+    // find reviews in list
+    if (movieReviews.id === id) {
+        res.status(200).json(movieReviews);
+    } else {
+        res.status(404).json({
+            message: 'The resource you requested could not be found.',
+            status_code: 404
+        });
+    }
+})
+moviesRouter.post('/:id/reviews',(req,res) => {
+    const id = parseInt(req.params.id);
+    if (movieReviews.id === id) {
+        req.body.created_at = new Date();
+        req.body.updated_at = new Date();
+        req.body.id = uniqid();
+        movieReviews.results.push(req.body); //push the new review onto the list
+        res.status(201).json(req.body);
+    } else {
+        res.status(404).json({
+            message: 'The resource you requested could not be found.',
+            status_code: 404
+        });
+    }
+})
 export default moviesRouter;
--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,7 @@ import moviesRouter from "./api/movies";
 dotenv.config();
 const app = express()
 const port = process.env.PORT
+app.use(express.json())
 app.use('/api/movies',moviesRouter);
 app.listen(port,()=>{
     console.info(`Server running at ${port}`);
