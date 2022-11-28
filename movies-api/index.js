ee9ccf9763c1f5b86196fd7f474bfd9363f34965
Hiroto Iwata <hiroto1610@outlook.com>
2022-11-28 16:39:23 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,7 @@ import moviesRouter from "./api/movies";
 import genresRouter from "./api/genres";
 import './db'
 import './seedData'
+import userRouter from "./api/users";
 
 dotenv.config();
 const app = express()
@@ -11,6 +12,7 @@ const port = process.env.PORT
 app.use(express.json())
 app.use('/api/movies',moviesRouter);
 app.use('/api/genres',genresRouter);
+app.use('/api/users',userRouter);
 app.listen(port,()=>{
     console.info(`Server running at ${port}`);
 });
