dd67ad03be06f60de3c84fab81f6a8a08f4c9bc4
Hiroto Iwata <hiroto1610@outlook.com>
2022-11-30 17:43:16 +0000

    Topic 10 - Update Movies Data/Route

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,22 +1,23 @@
 import express from "express";
 import {movies,movieDetails,movieReviews} from "./moviesData";
 import uniqid from 'uniqid'
+import movieModel from "./movieModel";
+import asyncHandler from "express-async-handler";
 
 const moviesRouter = express.Router();
-moviesRouter.get('/', (req, res) => {
-    res.json(movies);
-});
-moviesRouter.get('/:id',(req,res)=>{
+moviesRouter.get('/', asyncHandler(async (req, res) => {
+    const movies = await movieModel.find();
+    res.status(200).json(movies);
+}));
+moviesRouter.get('/:id',asyncHandler(async (req,res)=>{
     const id = parseInt(req.params.id);
-    if (movieDetails.id === id) {
-        res.status(200).json(movieDetails);
+    const movie = await movieModel.findByMovieDBId(id);
+    if (movie) {
+        res.status(200).json(movie);
     } else {
-        res.status(404).json({
-            message: 'The resource you requested could not be found.',
-            status_code: 404
-        });
+        res.status(404).json({message: 'The resource you requested could not be found.', status_code: 404});
     }
-});
+}));
 moviesRouter.get('/:id/reviews',(req,res) =>{
     const id = parseInt(req.params.id);
     // find reviews in list
--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -3,6 +3,8 @@ import users from './users';
 import dotenv from 'dotenv';
 import genreModel from "../api/genres/genreModel";
 import genres from "./genres";
+import movies from "./movies";
+import movieModel from "../api/movies/movieModel";
 
 dotenv.config();
 
@@ -27,8 +29,20 @@ async function loadGenres() {
         console.error(`failed to Load user Data: ${err}`);
     }
 }
-
+// deletes all movies documents in collection and inserts test data
+export async function loadMovies() {
+    console.log('load seed data');
+    console.log(movies.length);
+    try {
+        await movieModel.deleteMany();
+        await movieModel.collection.insertMany(movies);
+        console.info(`${movies.length} Movies were successfully stored.`);
+    } catch (err) {
+        console.error(`failed to Load movie Data: ${err}`);
+    }
+}
 if (process.env.SEED_DB) {
     loadUsers();
     loadGenres();
+    loadMovies();//ADD THIS LINE
 }
